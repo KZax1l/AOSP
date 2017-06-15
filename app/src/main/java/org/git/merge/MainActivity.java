d33e5a71293dbb4b7c950ee04a164abf8a9311e9
Zsago <3427241441@qq.com>
2017-06-15 23:25:14 +0800

    commit for the third time

--- app/src/main/java/org/git/merge/MainActivity.java
+++ app/src/main/java/org/git/merge/MainActivity.java
@@ -2,12 +2,18 @@ package org.git.merge;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
